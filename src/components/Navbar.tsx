1320b3e63ed0d2dcfe127208565582341b69b263
Noah Stewart <njstewart22@icloud.com>
2025-05-10 14:20:30 -0300

    Adding navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,27 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 function Navbar() {
-  return <div className="bg-peach font-quicksand text-4xl">Navbar</div>;
+  return (
+    <nav className="bg-cream p-4 flex justify-between items-center gap-4">
+      <Link href="/" className="flex items-center">
+        <Image src="/logo2.png" alt="Logo" width={96} height={96} />
+      </Link>
+      <ul className="flex space-x-8 text-peach mt-2 text-3xl text-right font-league-spartan font-bold">
+        <li className="hover:text-persimmon">
+          <Link href="/">About</Link>
+        </li>
+        <li className="hover:text-persimmon">
+          <Link href="/team">Team</Link>
+        </li>
+        <li className="hover:text-persimmon">
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li className="hover:text-persimmon">
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 export default Navbar;
