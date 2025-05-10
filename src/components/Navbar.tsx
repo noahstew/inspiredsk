abe5da6e4ad11fc1670d71c8b2675274d61361fc
Noah Stewart <njstewart22@icloud.com>
2025-05-10 15:17:02 -0300

    Updating nav styling with underlines

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,21 @@ import Link from 'next/link';
 
 function Navbar() {
   return (
-    <nav className="fixed left-0 top-0 w-full bg-cream p-4 flex justify-between items-center gap-4">
+    <nav className="w-full bg-cream p-4 flex justify-between items-center gap-4">
       <Link href="/" className="flex items-center">
         <Image src="/logo2.png" alt="Logo" width={96} height={96} />
       </Link>
-      <ul className="flex space-x-8 text-peach mt-2 text-3xl text-right font-league-spartan font-bold">
-        <li className="hover:text-persimmon">
+      <ul className="flex space-x-8 text-peach mt-4 mr-4 text-3xl text-right font-league-spartan font-bold">
+        <li className="hover:text-persimmon hover:underline">
           <Link href="/about">About</Link>
         </li>
-        <li className="hover:text-persimmon">
+        <li className="hover:text-persimmon hover:underline">
           <Link href="/team">Team</Link>
         </li>
-        <li className="hover:text-persimmon">
+        <li className="hover:text-persimmon hover:underline">
           <Link href="/blog">Blog</Link>
         </li>
-        <li className="hover:text-persimmon">
+        <li className="hover:text-persimmon hover:underline">
           <Link href="/contact">Contact</Link>
         </li>
       </ul>
