65915db59ad05bb5cb19119f060ce6a55455e4d4
Noah Stewart <njstewart22@icloud.com>
2025-05-10 15:16:44 -0300

    Updating footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,20 @@ import Link from 'next/link';
 
 function Footer() {
   return (
-    <div className="p-8 bg-pistachio">
-      Footer | Developed by{' '}
-      <Link target="_blank" href="https://nostew.com">
+    <footer className="p-8 border-t-2 border-persimmon bg-cream text-center text-peach text-lg font-league-spartan font-bold">
+      {/* <Link className="hover:text-persimmon" target="_blank" href="/admin">
+        InspirED
+      </Link>{' '}
+      |  */}
+      Developed & Maintained by{' '}
+      <Link
+        className="text-pistachio hover:text-olive"
+        target="_blank"
+        href="https://nostew.com"
+      >
         Noah Stewart
       </Link>{' '}
-    </div>
+    </footer>
   );
 }
 export default Footer;
