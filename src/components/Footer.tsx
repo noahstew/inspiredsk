ad251b8aa40afdc001550a69d2efdf5cd929df58
Noah Stewart <njstewart22@icloud.com>
2025-07-08 23:29:11 -0300

    Updating footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,21 @@
 import Link from 'next/link';
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="p-8 border-t-2 border-persimmon bg-cream text-center text-peach text-lg font-league-spartan font-bold">
+      &copy; {currentYear}{' '}
       <Link className="hover:text-persimmon" href="/admin">
         InspirED
       </Link>{' '}
-      | Developed & Maintained by{' '}
+      all rights reserved | developed & maintained by{' '}
       <Link
         className="text-pistachio hover:text-olive"
         target="_blank"
         href="https://nostew.com"
       >
-        Noah Stewart
+        nostew
       </Link>{' '}
     </footer>
   );
